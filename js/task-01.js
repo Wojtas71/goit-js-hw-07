ce1df2113793be4c7d0ad83b8e74a6b82fc70b2a
Wojtas71 <wojciech.foriasz@gmail.com>
2024-02-27 17:49:44 +0100

    task-01.js update

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,7 @@
 const listWithId = document.querySelector('#categories');
 const menuItemByTagName = listWithId.querySelectorAll(".item");
 console.log("Number of categories: " + menuItemByTagName.length);
+
+
+const list = document.querySelector('ul.categories');
+console.log(list);
