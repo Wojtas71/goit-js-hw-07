d51e240ff892c8e1a3ed4efc9990b024c008b8a0
Wojtas71 <wojciech.foriasz@gmail.com>
2024-02-28 10:41:07 +0100

    task-01.js visual errors repaired

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,7 @@
 const listWithId = document.querySelector('#categories');
 const menuItemByTagName = listWithId.querySelectorAll(".item");
 console.log("Number of categories: " + menuItemByTagName.length);
+console.log(" ");
 
 const totalItems = document.querySelectorAll('.item');
 totalItems.forEach((item) => {
@@ -8,4 +9,5 @@ totalItems.forEach((item) => {
     const totalPositionOfItem = item.lastElementChild.children.length;
     console.log("Category: " + titleItem);
     console.log("Elements: " + totalPositionOfItem);
+    console.log(" ");
 });
