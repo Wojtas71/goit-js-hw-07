b11005c33d2d1d8416365e4755798a2907e2b325
Wojtas71 <wojciech.foriasz@gmail.com>
2024-02-28 21:44:54 +0100

    task-02.js last file (wrong version) - this is final version

--- js/task-02.js
+++ js/task-02.js
@@ -7,14 +7,6 @@ const ingredients = [
   'Condiments',
 ];
 
-// const list = document.querySelector("#ingredients");
-// for(let i=0; i < ingredients.length; i++){
-//   const li = document.createElement("li");
-//   li.textContent = ingredients[i];
-//   list.append(li);
-// };
-
-
 const list = document.querySelector("#ingredients");
 const items = ingredients.map((item) => {
   const li = document.createElement("li");
