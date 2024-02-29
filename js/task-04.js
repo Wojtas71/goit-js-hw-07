c5458b6676a04e179f1cfa29a511cb8fde528641
Wojtas71 <wojciech.foriasz@gmail.com>
2024-02-29 15:51:52 +0100

    task-04.js testing version

--- js/task-04.js
+++ js/task-04.js
@@ -2,4 +2,32 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+};
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById('boxes');
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = '30px';
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
 }
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML = '';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const createBtn = document.querySelector('[data-create]');
+  const destroyBtn = document.querySelector('[data-destroy]');
+  
+  createBtn.addEventListener('click', () => {
+    const amount = parseInt(document.querySelector('input').value);
+    createBoxes(amount);
+  });
+  
+  destroyBtn.addEventListener('click', destroyBoxes);
+});
